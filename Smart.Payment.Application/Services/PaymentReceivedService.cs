c0b996699b8c0a7a5b15fa3b3d1a3091e584a81b
eglisonsouza <eglison.souza2019@gmail.com>
2024-04-17 07:39:37 -0300

    feat: add service from webhook

--- Smart.Payment.Application/Services/PaymentReceivedService.cs
+++ Smart.Payment.Application/Services/PaymentReceivedService.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using Smart.Payment.Application.Models;
 using Smart.Payment.Core.Models.Entities;
-using Smart.Payment.Core.Models.Enum;
 using Smart.Payment.Core.Repositories;
 
 namespace Smart.Payment.Application.Services
@@ -11,14 +10,15 @@ namespace Smart.Payment.Application.Services
         void AddEvent(PaymentReceivedAsaasModel paymentReceived);
     }
 
-    public class PaymentReceivedService(IPaymentRepository repository, IMapper mapper) : IPaymentReceivedService
+    public sealed class PaymentReceivedService(
+        IPaymentRepository repository,
+        IMapperBase mapper
+        ) : IPaymentReceivedService
     {
-        private readonly IPaymentRepository _repository = repository;
-        private readonly IMapper _mapper = mapper;
 
         public void AddEvent(PaymentReceivedAsaasModel paymentReceived)
         {
-            _repository.AddEvent(_mapper.Map<TransactionsEntity>(paymentReceived));
+            repository.AddEvent(mapper.Map<TransactionsEntity>(paymentReceived));
         }
     }
 }
---
+++ Smart.Payment.Infra/Persistence/Repositories/PaymentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Smart.Payment.Core.Models.Entities;
+using Smart.Payment.Core.Repositories;
+
+namespace Smart.Payment.Infra.Persistence.Repositories
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        public void AddEvent(TransactionsEntity payment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
