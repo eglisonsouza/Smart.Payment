2c5d4f64ac45a0a70cc356a8656d339892cf9d9e
eglisonsouza <eglison.souza2019@gmail.com>
2024-05-04 11:18:57 -0300

    refact: remove using

--- Smart.Payment.Application/Models/PaymentReceivedAsaasModel.cs
+++ Smart.Payment.Application/Models/PaymentReceivedAsaasModel.cs
@@ -1,9 +1,4 @@
 ﻿using Smart.Payment.Core.Models.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Smart.Payment.Application.Models
 {
